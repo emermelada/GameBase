20767bc9db870f98d32894a8869459db921ac1e1
Javier Arévalo <javier.arevalo2@live.u-tad.com>
2024-11-19 13:41:21 +0100

    Modularization of the register and log in fields.

---
+++ app/src/main/java/com/example/gamebase/ui/components/registerLogInField.kt
@@ -0,0 +1,24 @@
+package com.example.gamebase.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun registerLogInField(idString: Int, value: String, onChangeField: (String)->Unit){
+    Text(
+        text = stringResource(idString),
+        modifier = Modifier
+            .padding(10.dp)
+    )
+    TextField(
+        value = value,
+        onValueChange = { onChangeField(it)},
+        modifier = Modifier
+            .padding(8.dp)
+    )
+}
--- app/src/main/java/com/example/gamebase/ui/screens/register/SignUpScreen.kt
+++ app/src/main/java/com/example/gamebase/ui/screens/register/SignUpScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,12 +19,14 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.gamebase.R
 import com.example.gamebase.ui.components.gameBaseHeader
+import com.example.gamebase.ui.components.registerLogInField
 
 @Composable
 fun SignUpScreen(){
     var email by remember { mutableStateOf<String>("") }
     var username by remember { mutableStateOf<String>("") }
     var password by remember { mutableStateOf<String>("") }
+    var passwordCheck by remember { mutableStateOf<String>("") }
     Column{
         gameBaseHeader()
         Column(
@@ -37,55 +38,15 @@ fun SignUpScreen(){
                 elevation = CardDefaults.cardElevation(
                     defaultElevation = 9.dp)
             ){
-                Text(
-                    text = stringResource(R.string.emailSignUp),
-                    modifier = Modifier
-                        .padding(10.dp)
-                )
-                TextField(
-                    value = email,
-                    onValueChange = { email = it},
-                    modifier = Modifier
-                        .padding(8.dp)
-                )
-                Text(
-                    text = stringResource(R.string.usernameSignUp),
-                    modifier = Modifier
-                        .padding(10.dp)
-                )
-                TextField(
-                    value = username,
-                    onValueChange = { username = it},
-                    modifier = Modifier
-                        .padding(8.dp)
-                )
-                Text(
-                    text = stringResource(R.string.passwordSignUp),
-                    modifier = Modifier
-                        .padding(10.dp)
-                )
-                TextField(
-                    value = password,
-                    onValueChange = { password = it},
-                    modifier = Modifier
-                        .padding(8.dp)
-                )
-                Text(
-                    text = stringResource(R.string.passwordSignUpCheck),
-                    modifier = Modifier
-                        .padding(10.dp)
-                )
-                TextField(
-                    value = password,
-                    onValueChange = { password = it},
-                    modifier = Modifier
-                        .padding(8.dp)
-                )
+                registerLogInField(R.string.emailSignUp, email) { it: String -> email = it }
+                registerLogInField(R.string.usernameSignUp, username) { it: String -> username = it }
+                registerLogInField(R.string.passwordSignUp, password) { it: String -> password = it }
+                registerLogInField(R.string.passwordSignUpCheck, passwordCheck) { it: String -> passwordCheck = it }
             }
             Button(
                 modifier = Modifier
                     .padding(8.dp),
-                onClick = {}
+                onClick = { print("Email: ${email}, Usuario: ${username}, Email: ${password}.") }
             ) {
                 Text(text = stringResource(R.string.signUpButton))
             }

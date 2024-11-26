753a70b80aa0305e02b9d007a8a982c0f6c1e7c1
Javier Arévalo <javier.arevalo2@live.u-tad.com>
2024-11-26 10:44:30 +0100

    More changes on the scaffold.

--- app/src/main/java/com/example/gamebase/ui/components/MainScaffold.kt
+++ app/src/main/java/com/example/gamebase/ui/components/MainScaffold.kt
@@ -1,8 +1,11 @@
 package com.example.gamebase.ui.components
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,15 +15,28 @@ import androidx.compose.ui.unit.dp
 import com.example.gamebase.ui.screens.HomeAppScreen
 import com.example.gamebase.ui.viewmodels.GameListViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScaffold(gameListViewModel: GameListViewModel) {
     Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                modifier = Modifier
+                    .height(40.dp),
+                title = {
+                    Text("GameBase")
+                }
+            )
+        },
         bottomBar = {
             BottomAppBar(
                 containerColor = Color.White,
-                modifier = Modifier.padding(1.dp)
+                modifier = Modifier
+                    .padding(0.dp)
+                    .height(50.dp)
             ){
-                Button(onClick = {}) {
+                Button(onClick = {},
+                    modifier = Modifier.padding(0.dp)) {
                     Text("Hola")
                 }
             }
